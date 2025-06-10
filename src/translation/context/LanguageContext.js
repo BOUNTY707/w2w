53ccf40bde688cf4823b4a942c76355132168d40
BOUNTY707 <mexrobakooo@gmail.com>
2025-06-10 12:10:58 +0500

    Update added

--- src/translation/context/LanguageContext.js
+++ src/translation/context/LanguageContext.js
@@ -23,30 +23,3 @@ export const LanguageProvider = ({ children }) => {
     </LanguageContext.Provider>
   );
 };
-
-
-
-
-
-
-// import { createContext, useState, useEffect } from "react";
-// import en from "../en.json";
-// import ru from "../ru.json";
-
-// export const LanguageContext = createContext();
-
-// export const LanguageProvider = ({ children }) => {
-//   const [language, setLanguage] = useState(localStorage.getItem("lang") || "en");
-//   const [translations, setTranslations] = useState(language === "en" ? en : ru);
-
-//   useEffect(() => {
-//     localStorage.setItem("lang", language);
-//     setTranslations(language === "en" ? en : ru);
-//   }, [language]);
-
-//   return (
-//     <LanguageContext.Provider value={{ language, setLanguage, translations }}>
-//       {children}
-//     </LanguageContext.Provider>
-//   );
-// };
