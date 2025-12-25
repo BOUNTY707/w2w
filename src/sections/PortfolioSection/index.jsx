63edddd0519253f0332ee1442f0fea025c1d1658
BOUNTY707 <mexrobakooo@gmail.com>
2025-12-25 18:33:47 +0500

    Portfolio update

--- src/sections/PortfolioSection/index.jsx
+++ src/sections/PortfolioSection/index.jsx
@@ -219,8 +219,8 @@ export default function PortfolioSection() {
             <SwiperSlide>
               <div className="slide_card">
                 <iframe
-                  src="https://ujlinks.uz/"
-                  title="ujlinks.uz"
+                  src="https://salesdoc.io"
+                  title="salesdoc.io"
                   className="site_iframe"
                   loading="lazy"
                 ></iframe>
